1769b3871386600f29b221c817c35a094b89f33a
Chris Burton <chris.burton@revolutionbarsgroup.com>
2025-06-08 18:27:59 +0100

    Change date time to string value

--- src/Application/Traits/ArrayableTrait.php
+++ src/Application/Traits/ArrayableTrait.php
@@ -6,6 +6,13 @@ trait ArrayableTrait
 {
     public function toArray(): array
     {
-        return get_object_vars($this);
+        $vars = [];
+        foreach (get_object_vars($this) as $key => $value) {
+            if ($value instanceof \DateTimeInterface) {
+                $value = $value->format('Y-m-d H:i:s');
+            }
+            $vars[$key] = $value;
+        }
+        return $vars;
     }
 }
